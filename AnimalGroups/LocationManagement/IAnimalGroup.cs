3ad0857cdcb3e42734b248bcee38d53db82d5e13
DarinaVT <darito.timoteeva10@gmail.com>
2025-04-28 17:32:22 +0300

    Delete AnimalGroups directory

--- AnimalGroups/LocationManagement/IAnimalGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AnimalFight;
-
-public interface IAnimalGroup
-{
-    List<Animal> Animals { get; }
-    abstract List<Animal> GetAnimals();
-    public ContinentType Continent { get; }
-    public EnvironmentType Environment { get; }
-}
